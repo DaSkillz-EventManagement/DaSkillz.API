6fc1afc3e23dceb0eb5dad0f2475f04ef5cfd261
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2024-09-01 11:47:45 +0700

    define structure

---
+++ Infrastructure/ExternalServices/ElasticSearch/Setting/ElasticSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Persistence.Elasticsearch.Setting
+{
+    public class ElasticSetting
+    {
+        public string Url { get; set; } = string.Empty;
+        public string? DefaultIndex { get; set; }
+    }
+}
