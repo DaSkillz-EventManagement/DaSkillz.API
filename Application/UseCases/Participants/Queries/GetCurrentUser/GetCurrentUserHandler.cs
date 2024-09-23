a083402e455a6ec4affebbb531db4ddca3e76e06
Minhduc027 <minhduylongthuan@gmail>
2024-09-24 01:49:41 +0700

    update again

--- Application/UseCases/Participants/Queries/GetCurrentUser/GetCurrentUserHandler.cs
+++ Application/UseCases/Participants/Queries/GetCurrentUser/GetCurrentUserHandler.cs
@@ -1,31 +1,23 @@
 ﻿using AutoMapper;
 using Domain.DTOs.ParticipantDto;
-using Domain.Entities;
 using Domain.Repositories;
-using Domain.Repositories.UnitOfWork;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Application.UseCases.Participants.Queries.GetCurrentUser
+namespace Application.UseCases.Participants.Queries.GetCurrentUser;
+
+public class GetCurrentUserHandler : IRequestHandler<GetCurrentUserQueries, ParticipantEventDto>
 {
-    public class GetCurrentUserHandler : IRequestHandler<GetCurrentUserQueries, ParticipantEventDto>
+    private readonly IParticipantRepository _participantRepository;
+    private readonly IMapper _mapper;
+    public GetCurrentUserHandler(IParticipantRepository participantRepository, IMapper mapper)
     {
-        private readonly IParticipantRepository _participantRepository;
-        private readonly IMapper _mapper;
-        public GetCurrentUserHandler(IParticipantRepository participantRepository, IMapper mapper)
-        {
-            _participantRepository = participantRepository;
-            _mapper = mapper;
-        }
-        public async Task<ParticipantEventDto> Handle(GetCurrentUserQueries request, CancellationToken cancellationToken)
-        {
-            var user = await _participantRepository.GetDetailParticipant(request.UserId, request.EventId);
+        _participantRepository = participantRepository;
+        _mapper = mapper;
+    }
+    public async Task<ParticipantEventDto> Handle(GetCurrentUserQueries request, CancellationToken cancellationToken)
+    {
+        var user = await _participantRepository.GetDetailParticipant(request.UserId, request.EventId);
 
-            return _mapper.Map<ParticipantEventDto>(user);
-        }
+        return _mapper.Map<ParticipantEventDto>(user);
     }
 }
