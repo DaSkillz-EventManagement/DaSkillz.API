830d2d3d05db4b3f434f6a134aa263483afeb3ac
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2024-09-25 13:38:39 +0700

    change business payment

--- Application/UseCases/Payment/Commands/CreatePayment/CreatePayment.cs
+++ Application/UseCases/Payment/Commands/CreatePayment/CreatePayment.cs
@@ -8,7 +8,7 @@ namespace Application.UseCases.Payment.Commands.CreatePayment
         public Guid UserId { get; set; }
         public string? Description { get; set; }
         public string Amount { get; set; }
-        public string? EventId { get; set; }
+        public Guid? EventId { get; set; }
         public int SubscriptionType { get; set; }
         public string? redirectUrl { get; set; }
     }
--- Application/UseCases/Payment/Queries/GetOrderStatus/GetOrderQueryQueryHandler.cs
+++ Application/UseCases/Payment/Queries/GetOrderStatus/GetOrderQueryQueryHandler.cs
@@ -1,17 +1,20 @@
 ﻿using Application.Abstractions.Caching;
+using Application.Abstractions.Email;
 using Application.Abstractions.Payment.ZaloPay;
-using Application.Helper.ZaloPayHelper.Crypto;
+using Application.Helper;
+using Application.ResponseMessage;
+using Domain.Constants.Mail;
+using Domain.DTOs.ParticipantDto;
+using Domain.Entities;
+using Domain.Enum.Events;
+using Domain.Enum.Participant;
 using Domain.Enum.Payment;
 using Domain.Models.Response;
 using Domain.Repositories;
 using Domain.Repositories.UnitOfWork;
+using Elastic.Clients.Elasticsearch.Security;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZaloPay.Helper;
+using System.Net;
 
 namespace Application.UseCases.Payment.Queries.GetOrderStatus
 {
@@ -19,12 +22,34 @@ namespace Application.UseCases.Payment.Queries.GetOrderStatus
     {
         private readonly IZaloPayService _zaloPayService;
         private readonly ITransactionRepository _transactionRepository;
+        private readonly IParticipantRepository _participantRepository;
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly ISponsorEventRepository _sponsorEventRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IEventRepository _eventRepository;
+        private readonly IEmailService _emailService;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IRedisCaching _caching;
-        public GetOrderQueryQueryHandler(IZaloPayService zaloPayService, ITransactionRepository transactionRepository, IUnitOfWork unitOfWork, IRedisCaching caching)
+
+        public GetOrderQueryQueryHandler(IZaloPayService zaloPayService,
+            ITransactionRepository transactionRepository,
+            IParticipantRepository participantRepository,
+            ISubscriptionRepository subscriptionRepository,
+            ISponsorEventRepository sponsorEventRepository,
+            IUserRepository userRepository,
+            IEventRepository eventRepository,
+            IEmailService emailService,
+            IUnitOfWork unitOfWork,
+            IRedisCaching caching)
         {
             _zaloPayService = zaloPayService;
             _transactionRepository = transactionRepository;
+            _participantRepository = participantRepository;
+            _subscriptionRepository = subscriptionRepository;
+            _sponsorEventRepository = sponsorEventRepository;
+            _userRepository = userRepository;
+            _eventRepository = eventRepository;
+            _emailService = emailService;
             _unitOfWork = unitOfWork;
             _caching = caching;
         }
@@ -33,18 +58,124 @@ namespace Application.UseCases.Payment.Queries.GetOrderStatus
         {
             var result = await _zaloPayService.QueryOrderStatus(request.appTransId!);
             var exist = await _transactionRepository.GetById(request.appTransId!);
-            if (exist != null)
-            {
-                exist.Zptransid = result["zp_trans_id"].ToString();
-                if (!(bool)result["is_processing"])
-                {
-                    exist.Status = (int)TransactionStatus.SUCCESS;
-                }
-                await _transactionRepository.Update(exist);
-                await _unitOfWork.SaveChangesAsync();
 
-                await _caching.RemoveAsync($"payment_{request.appTransId}");
+            if (exist == null)
+            {
+                return new APIResponse
+                {
+                    StatusResponse = System.Net.HttpStatusCode.NotFound,
+                    Message = "Can't find this transaction",
+                    Data = null,
+                };
             }
+
+            exist.Zptransid = result["zp_trans_id"].ToString();
+            if ((int)result["return_code"] == 1)
+            {
+                exist.Status = (int)TransactionStatus.SUCCESS;
+                if (exist.SubscriptionType == (int)PaymentType.TICKET)
+                {
+                    var isExistedOnEvent = await _participantRepository.IsExistedOnEvent((Guid)exist.UserId!, (Guid)exist.EventId!);
+
+                    if (isExistedOnEvent)
+                    {
+                        return new APIResponse()
+                        {
+                            StatusResponse = HttpStatusCode.Conflict,
+                            Message = MessageParticipant.ExistedOnEvent,
+                            Data = null
+                        };
+                    }
+                    Participant participant = new()
+                    {
+                        UserId = (Guid)exist.UserId!,
+                        EventId = (Guid)exist.EventId!,
+                        RoleEventId = (int)EventRole.Visitor + 1,
+                        CreatedAt = DateTime.Now,
+                        IsCheckedMail = false,
+                        Status = ParticipantStatus.Confirmed.ToString()
+                    };
+
+                    await _participantRepository.Add(participant);
+
+                    var currentEvent = await _eventRepository.GetById(exist.EventId);
+                    var Owner = await _userRepository.GetById(currentEvent!.CreatedBy!);
+                    var user = await _userRepository.GetById(exist.UserId);
+                    await _emailService.SendEmailTicket(MailConstant.TicketMail.PathTemplate, MailConstant.TicketMail.Title, new TicketModel()
+                    {
+                        EventId = (Guid)exist.EventId,
+                        UserId = (Guid)currentEvent.CreatedBy!,
+                        Email = user!.Email,
+                        RoleEventId = (int)EventRole.Visitor + 1,
+                        FullName = user.FullName,
+                        Avatar = Owner!.Avatar,
+                        EventName = currentEvent?.EventName,
+                        Location = currentEvent?.Location,
+                        LocationUrl = currentEvent?.LocationUrl,
+                        LocationAddress = currentEvent?.LocationAddress,
+                        LogoEvent = currentEvent?.Image,
+                        OrgainzerName = Owner.FullName,
+                        StartDate = DateTimeOffset.FromUnixTimeMilliseconds(currentEvent!.StartDate).DateTime,
+                        EndDate = DateTimeOffset.FromUnixTimeMilliseconds(currentEvent!.EndDate).DateTime,
+                        Time = DateTimeHelper.GetTimeRange(DateTimeOffset.FromUnixTimeMilliseconds(currentEvent.StartDate).DateTime, DateTimeOffset.FromUnixTimeMilliseconds(currentEvent.EndDate).DateTime),
+                        Message = TicketMailConstant.MessageMail.ElementAt((int)EventRole.Visitor),
+                        TypeButton = Utilities.GetTypeButton((int)EventRole.Visitor + 1),
+                    });
+                }
+                else if (exist.SubscriptionType == (int)PaymentType.SUBSCRIPTION)
+                {
+                    var subscription = await _subscriptionRepository.GetByUserId(exist.UserId);
+                    if (subscription != null)
+                    {
+                        if (subscription.EndDate >= DateTime.UtcNow)
+                        {
+                            subscription.EndDate = subscription.EndDate.AddMonths(1);
+                        }
+                        else
+                        {
+                            subscription.StartDate = DateTime.UtcNow;
+                            subscription.EndDate = DateTime.UtcNow.AddMonths(1);
+                        }
+                        subscription.IsActive = true;
+                    }
+                    else
+                    {
+                        subscription = new Subscription
+                        {
+                            UserId = (Guid)exist.UserId!,
+                            StartDate = DateTime.UtcNow,
+                            EndDate = DateTime.UtcNow.AddMonths(1),
+                            IsActive = true
+                        };
+                        await _subscriptionRepository.Add(subscription);
+                    }
+                }
+                else if (exist.SubscriptionType == (int)PaymentType.SPONSOR)
+                {
+                    var sponsor = await _sponsorEventRepository.CheckSponsoredEvent((Guid)exist.EventId!, (Guid)exist.UserId!);
+                    if (sponsor == null)
+                    {
+                        return new APIResponse
+                        {
+                            StatusResponse = HttpStatusCode.NotFound,
+                            Message = "Can't find this sponsor event",
+                            Data = null,
+                        };
+                    }
+                    sponsor.IsSponsored = true;
+                }
+            }
+            else if ((int)result["return_code"] == 2)
+            {
+                exist.Status = (int)TransactionStatus.FAIL;
+            }
+            await _transactionRepository.Update(exist);
+            await _unitOfWork.SaveChangesAsync();
+
+            await _caching.RemoveAsync($"payment_{request.appTransId}");
+
+
+
             return new APIResponse
             {
                 StatusResponse = System.Net.HttpStatusCode.OK,
