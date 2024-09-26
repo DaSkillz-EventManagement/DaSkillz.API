0aac9aa4070f09b3225c6d0a5c3b7d6413041b5b
Minhduc027 <minhduylongthuan@gmail>
2024-09-26 07:50:35 +0700

    Fix bug add user to event

--- Domain/Constants/Mail/TicketMailConstant.cs
+++ Domain/Constants/Mail/TicketMailConstant.cs
@@ -5,9 +5,9 @@ public class TicketMailConstant
     public static IEnumerable<string> MessageMail = new List<string>
         {
             "You have registered for",
-            "You are an Sponsor for",
+            "You are a Sponsor for",
             "You are an Event Operator for",
-            "You are an Checking Staff for",
+            "You are a Checking Staff for",
             "You have registered for",
             "Your sponsor request has been rejected by the host of event"
         };
