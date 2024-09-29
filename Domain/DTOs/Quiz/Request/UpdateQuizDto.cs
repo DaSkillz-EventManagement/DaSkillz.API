8996193ff76de91f270480fc188b98c0b1f31bf4
Minhduc027 <minhduylongthuan@gmail>
2024-09-29 20:11:33 +0700

    fix bug updatequiz

--- Domain/DTOs/Quiz/Request/UpdateQuizDto.cs
+++ Domain/DTOs/Quiz/Request/UpdateQuizDto.cs
@@ -4,6 +4,7 @@ namespace Domain.DTOs.Quiz.Request;
 public class UpdateQuizDto
 {
     public Guid QuizId { get; set; }
+    public Guid EventId { get; set; }
     public string QuizName { get; set; } = null!;
     public string QuizDescription { get; set; } = null!;
     public string TotalTime { get; set; } = null!;
