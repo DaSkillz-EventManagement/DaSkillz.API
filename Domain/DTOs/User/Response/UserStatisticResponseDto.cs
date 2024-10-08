074689dc8974d0d895f925f339881952c71ef149
CongLam1806 <lamtc1806@gmail.com>
2024-10-08 23:11:17 +0700

    update

---
+++ Domain/DTOs/User/Response/UserStatisticResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.User.Response
+{
+    public class UserStatisticResponseDto
+    {
+        public Guid UserId { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string Status { get; set; } = null!;
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; }
+        public string? Avatar { get; set; }
+        public bool? IsPremiumUser { get; set; } = false;
+        public int NumOfPurchasing { get; set; }
+        public decimal TotalMoney { get; set; }
+    }
+}
