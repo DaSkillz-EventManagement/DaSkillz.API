43ec43446ce1cc98f7d8f9447e36b88f7db85f82
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2024-10-30 16:08:05 +0700

    minor change

--- Domain/Constants/Domain/DomainName.cs
+++ Domain/Constants/Domain/DomainName.cs
@@ -2,7 +2,7 @@
 {
     public class DomainName
     {
-        public const string DOMAIN_NAME = "daskillz-g9h7g9cpcrghamby.southeastasia-01.azurewebsites.net";
+        public const string DOMAIN_NAME = "daskillzz-bxckcfdya2bhf5ff.southeastasia-01.azurewebsites.net";
         public const string LOCAL = "localhost:60098";
         public const string LOCATION_URL = $"https://{LOCAL}/api/v1/redirect";
         public const string PRODUCTION_URL = $"https://{DOMAIN_NAME}/api/v1/redirect";
